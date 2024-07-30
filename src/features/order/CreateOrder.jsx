fb7e4d8168a3ec37d4e625060a308fd6e68f6466
yurliansyahf <yurliansyahf@gmail.com>
2024-07-30 22:05:54 +0700

    add reactrouterdom and setup page routes

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 // https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+// const isValidPhone = (str) =>
+//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+//     str
+//   );
 
 const fakeCart = [
   {
@@ -12,31 +12,31 @@ const fakeCart = [
     name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
-    totalPrice: 32,
+    totalPrice: 32
   },
   {
     pizzaId: 6,
     name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
-    totalPrice: 13,
+    totalPrice: 13
   },
   {
     pizzaId: 11,
     name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
-    totalPrice: 15,
-  },
+    totalPrice: 15
+  }
 ];
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
-
+  console.log(cart);
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Lets go!</h2>
 
       <form>
         <div>
