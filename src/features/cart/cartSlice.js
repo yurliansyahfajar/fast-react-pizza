7c030319bdb0cc1e8797e776e27794222f048dd2
yurliansyahf <yurliansyahf@gmail.com>
2024-08-04 18:06:05 +0700

    setup cart reducer and cart slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 11,
+      name: "Spinach and Mushroom",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+    },
+    {
+      pizzaId: 6,
+      name: "Vegetale",
+      quantity: 1,
+      unitPrice: 13,
+      totalPrice: 13,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload is new pizza object
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload is pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      //payload is pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      //payload is pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
